656e9370eb3e4c83bf610bcdf13e8416b0146eb6
pefish <pefish@qq.com>
2021-03-12 23:09:51 +0800

    add

--- cmd/eth-timelock-monitor/command/default.go
+++ cmd/eth-timelock-monitor/command/default.go
@@ -30,6 +30,7 @@ func (dc *DefaultCommand) DecorateFlagSet(flagSet *flag.FlagSet) error {
 	flagSet.String("contract-address", "0xA1f482Dc58145Ba2210bC21878Ca34000E2e8fE4", "contract address")
 	flagSet.String("telegram-token", "", "telegram token")
 	flagSet.String("chat-id", "", "chat id of group")
+	flagSet.String("notify", "", "notify")
 	return nil
 }
 
@@ -38,27 +39,35 @@ func (dc *DefaultCommand) OnExited(data *commander.StartData) error {
 }
 
 func (dc *DefaultCommand) Start(data *commander.StartData) error {
-	wsServer, err := go_config.Config.GetString("ws-server")
+	wsServer, err := go_config.ConfigManagerInstance.GetString("ws-server")
 	if err != nil {
 		return err
 	}
-	contractAddress, err := go_config.Config.GetString("contract-address")
+	contractAddress, err := go_config.ConfigManagerInstance.GetString("contract-address")
 	if err != nil {
 		return err
 	}
-	telegramToken, err := go_config.Config.GetString("telegram-token")
+	telegramToken, err := go_config.ConfigManagerInstance.GetString("telegram-token")
 	if err != nil {
 		return err
 	}
 	if telegramToken == "" {
 		return errors.New("telegram token must be set")
 	}
-	chatIdStr, err := go_config.Config.GetString("chat-id")
+	chatIdStr, err := go_config.ConfigManagerInstance.GetString("chat-id")
 	if err != nil {
 		return err
 	}
 	if chatIdStr == "" {
 		return errors.New("chatId must be set")
+	}
+
+	listenMethods := make([]string, 0)
+	go_config.ConfigManagerInstance.MustGet("listen-methods", &listenMethods)
+
+	notifyStr, err := go_config.ConfigManagerInstance.GetString("notify")
+	if err != nil {
+		return err
 	}
 	chatId, err := go_reflect.Reflect.ToInt64(chatIdStr)
 	if err != nil {
@@ -100,7 +109,7 @@ func (dc *DefaultCommand) Start(data *commander.StartData) error {
 		for range timer.C {
 			err := telegramRobot.TelegramSender().SendMsg(telegram_sender.MsgStruct{
 				ChatId: chatId,
-				Msg:    []byte(fmt.Sprintf("监控中，一切正常（合约地址：%s）", contractAddress)),
+				Msg:    fmt.Sprintf("监控中，一切正常（合约地址：%s）", contractAddress),
 			}, 0)
 			if err != nil {
 				go_logger.Logger.Error(err)
@@ -113,18 +122,24 @@ func (dc *DefaultCommand) Start(data *commander.StartData) error {
 		select {
 		case result := <- resultChan:
 			methodStr := result["signature"].(string)
-			if strings.Contains(methodStr, "setMigrator") {
+			isDead := false
+			for _, listenMethod := range listenMethods {
+				if strings.Contains(methodStr, listenMethod) {
+					isDead = true
+				}
+			}
+			if isDead {
 				methodStr = fmt.Sprintf(`
-⚠️紧急通知：%s被调用，迁移合约变动，资产风险非常高，为确保资产安全，请立刻联系陈旭（手机号：13575724011）提出pancake所有资产，并联系继勇（手机号：18317042249）查看情况
-`, methodStr)
+⚠️紧急通知： %s 被调用，迁移合约变动，资产风险非常高，为确保资产安全，请立刻联系陈旭（手机号：13575724011）提出 pancake 所有资产，并联系继勇（手机号：18317042249）查看情况
+ ` + notifyStr, methodStr)
 			} else {
 				methodStr = fmt.Sprintf(`
-通知：%s被调用，但无风险，无需任何动作，工作日提醒继勇确认即可
+通知： %s 被调用，但无风险，无需任何动作，工作日提醒继勇确认即可
 `, methodStr)
 			}
 			err := telegramRobot.TelegramSender().SendMsg(telegram_sender.MsgStruct{
 				ChatId: chatId,
-				Msg:    []byte(methodStr),
+				Msg:    methodStr,
 			}, 0)
 			if err != nil {
 				return err
--- go.mod
+++ go.mod
@@ -3,12 +3,11 @@ module github.com/pefish/eth-timelock-monitor
 go 1.14
 
 require (
-	github.com/pefish/go-build-tool v0.0.6 // indirect
 	github.com/pefish/go-coin-eth v0.8.19
-	github.com/pefish/go-commander v0.3.11
-	github.com/pefish/go-config v0.2.1
+	github.com/pefish/go-commander v0.4.1
+	github.com/pefish/go-config v0.2.2
 	github.com/pefish/go-logger v0.4.14
 	github.com/pefish/go-reflect v0.2.2
-	github.com/pefish/telegram-bot-manager v0.1.4
+	github.com/pefish/telegram-bot-manager v0.1.5
 	github.com/pkg/errors v0.9.1
 )
